25262b799c0aca9ce89a211fbb46e43fc20b4258
Kostya Serov | Kamerlingh Onnes <3007651@leerling.o2g2.nl>
2023-12-04 13:29:33 +0100

    basic redirects added

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from flask import Flask, render_template
 app = Flask(__file__)
 @app.route("/")
 def home():
-    return  "hi"
+    return  render_template("sport_main.html")
+@app.route("/over_ons")
+def about_us():
+    return render_template("sport (1).html")
 
 
 
