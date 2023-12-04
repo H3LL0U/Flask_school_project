d7f3852f1998dcf46e143b05ff182b34fe94e6c2
Konstantin <turbo.foxy01@gmail.com>
2023-12-04 17:47:23 +0100

    implomented all of the files from static

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 app = Flask(__file__)
 @app.route("/")
 def home():
-    return  render_template("sport_main.html")
+    return render_template("sport_main.html")
 @app.route("/over_ons")
 def about_us():
     return render_template("sport (1).html")
--- static/scripts/pop-up.js
+++ static/scripts/pop-up.js
@@ -1,5 +1,5 @@
 document.getElementById("close-pop-up").onclick = function(){
 
     document.getElementsByClassName("pop-up")[0].style.display = "none";
-    
+    console.log("aaaa")
 }
