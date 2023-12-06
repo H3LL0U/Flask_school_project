a400989b6f3ec4c848765e816c6f8979fc6aff1a
Konstantin <turbo.foxy01@gmail.com>
2023-12-06 23:20:32 +0100

    fully functional buttons implomented with the use of jinja

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template
 app = Flask(__file__)
 @app.route("/")
 def home():
-    return render_template("sport_main.html")
+    return render_template("sport_main.html", root_name = "/")
 @app.route("/over_ons")
 def about_us():
-    return render_template("sport (1).html")
+    return render_template("sport (1).html", root_name = "/over_ons")
 
 
 
